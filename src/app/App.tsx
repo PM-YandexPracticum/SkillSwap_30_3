189a5aacc73ec6409fd865d42a5de9e6ac8d2b14
RuslanWeber <ruslan26812@gmail.com>
2025-06-21 15:13:56 +0300

    add AppHeaderConnector

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,50 +1,22 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import styles from './app.module.css';
 import { RegistrationPage } from '@/pages/RegistrationPage/RegistrationPage';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProtectedRoute } from './providers/ProtectedRoute';
-import { Header } from '../shared/ui/app-header/app-header';
-import { AllSkillsModal } from '../shared/ui/AllSkillsModal';
-import { categories } from '../shared/ui/AllSkillsModal/mockCategories';
 
 const App = () => {
-  const [isSkillsModalVisible, setSkillsModalVisible] = useState(false);
   const isAuthenticated = false;
 
-  const handleSkillsToggle = useCallback(() => {
-    setSkillsModalVisible(true);
-  }, []);
-
-  const handleCloseSkillsModal = useCallback(() => {
-    setSkillsModalVisible(false);
-  }, []);
-
   return (
-    <div className={styles.app}>
-      <Header
-        isAuthenticated={isAuthenticated}
-        userName=""
-        avatarUrl=""
-        onLogin={() => {}}
-        onRegister={() => {}}
-        onThemeToggle={() => {}}
-        onNotificationsClick={() => {}}
-        onFavoritesClick={() => {}}
-        onSkillsToggle={handleSkillsToggle}
-      />
-
-      <AllSkillsModal
-        visible={isSkillsModalVisible}
-        categories={categories}
-        onClose={handleCloseSkillsModal}
-      />
-
-      <Routes>
-        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
-          <Route path="/registration" element={<RegistrationPage />} />
-        </Route>
-      </Routes>
-    </div>
+    <Router>
+      <div className={styles.app}>
+        <Routes>
+          <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+            <Route path="/registration" element={<RegistrationPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
